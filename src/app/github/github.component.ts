68fc73c48d3335e0abf1faac625ee364aaf82dde
julkwel <julienrajerison5@gmail.com>
2022-08-20 22:45:53 +0300

    Type of variables

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 
 export class GithubComponent implements OnInit {
   data: any = [];
-  userCount: Number = 0;
+  userCount: number = 0;
   error: Boolean = false;
   defaultLocation: string = 'madagascar';
   displayedColumns: string[] = ['login', 'avatar_url'];
